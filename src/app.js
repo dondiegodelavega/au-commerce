f16ce141af12587da748551cbbfee961aac68fa6
Delavega <eabluebirdea@gmail.com>
2017-04-20 12:35:19 +0800

    basic update for cli 0.28 and starting to make folder structure

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
+  configureRouter(config, router) {
+    config.title = 'Jazz & Co';
+    config.map([
+            { route: '',              moduleId: './pages/home/home',    name: 'home', title: 'Home'},
+            { route: 'catalog/:id',   moduleId: './pages/home/catalog', name: 'catalog'},
+            { route: 'content/:id',   moduleId: './pages/home/content', name: 'content'}
+    ]);
+
+    this.router = router;
   }
 }
